dd50db18037e45b2004e74de628dd18bfc0270be
Moccachino <daniel.groeger@outlook.com>
2024-07-20 14:37:58 +0200

    Revert "installed shadcn"
    This reverts commit b92ec2a91a3448157c314b04625e88ec00e12120.

--- templates/website/src/app/components/Header/index.client.tsx
+++ templates/website/src/app/components/Header/index.client.tsx
@@ -35,7 +35,10 @@ export const HeaderClient: React.FC<HeaderClientProps> = ({ header }) => {
       {...(theme ? { 'data-theme': theme } : {})}
     >
       <Link href="/">
-        <Logo />
+        <div className="flex items-center">
+          <Logo />
+          <h1 className="text-2xl text-white">Moccassins</h1>
+        </div>
       </Link>
       <HeaderNav header={header} />
     </header>
